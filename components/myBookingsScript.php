8ce632551110eec856986bf2afea2a969d2db956
Soizic Venel <venel.soizic@hotmail.fr>
2022-03-21 17:54:54 +0100

    add myBookingsScript.php / function delete booking

---
+++ components/myBookingsScript.php
@@ -0,0 +1,23 @@
+<?php
+if(!empty($_GET['delete']) && intval($_GET['delete'])){
+    $clientID = $_SESSION['userId'];
+    $bookingId = htmlspecialchars($_GET['delete']);
+    $bookingReq = $bdd->prepare('SELECT * FROM bookings WHERE id = :id AND client_id = :cid');
+    $bookingReq->bindValue(':id', $bookingId, PDO::PARAM_INT);
+    $bookingReq->bindValue(':cid', $clientID, PDO::PARAM_INT);
+    $bookingReq->execute();
+    $bCount = $bookingReq->rowCount();
+    if($bCount == 1){
+        $bookingInfos = $bookingReq->fetch(PDO::FETCH_ASSOC);
+        $now = date('Y-m-d');
+        if($now < $bookingInfos['cancellationDate']){
+            $bDeleteReq = $bdd->prepare('DELETE FROM bookings WHERE id = :id;');
+            $bDeleteReq->bindValue(':id', $bookingId, PDO::PARAM_INT);
+            $bDeleteReq->execute();
+        } else {
+            header('location:./myBookings.php?error=invalid');
+        }
+    } else {
+        header('location:./myBookings.php?error=invalid');
+    }
+}
--- myBookings.php
+++ myBookings.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once('./components/db/db.php');
+require_once('./components/myBookingsScript.php');
 if(isset($_SESSION['connect'])){
     if($_SESSION['connect'] == 'client'){
         require_once('./components/header/header-client.php');
@@ -20,7 +21,7 @@ if(isset($_SESSION['connect'])){
         <img class="img-fluid" src="./assets/img/hypnos-hotel-flower.svg" alt="illustration, fleurs">
     </div>
     <div class="col-12 col-md-6">
-        <h2 class="text-tilered mb-3">Mes réservations</h2>
+        <h2 class="text-tilered mb-5">Mes réservations</h2>
         <h4 class="text-gold mb-3">Bonjour <?= $_SESSION['user'] ?> !</h4>
         <p>Retrouvez sur cette page l’historique de vos réservations dans nos hôtels Hypnos ! <br>Votre satisfaction étant au centre de nos préoccupations, n’hésitez pas à nous contacter en cas de besoin.</p>
     </div>
@@ -34,7 +35,7 @@ if(isset($_SESSION['connect'])){
         $bookingsReq = $bdd->prepare('SELECT bookings.id AS id, 
                                     DATE_FORMAT(bookings.startDate, "%d/%m/%Y") AS startDate, 
                                     DATE_FORMAT(bookings.endDate, "%d/%m/%Y" ) AS endDate,
-                                    DATE_FORMAT(bookings.cancellationDate, "%d/%m/%Y" ) AS cancellationDate, 
+                                   /*  DATE_FORMAT(bookings.cancellationDate, "%d/%m/%Y" ) AS  */bookings.cancellationDate, 
                                     suites.title, 
                                     hotels.name
                                     FROM bookings 
@@ -44,30 +45,35 @@ if(isset($_SESSION['connect'])){
                                     ORDER BY startDate DESC;');
         $bookingsReq->bindValue(':cid', $clientId, PDO::PARAM_INT);
         $bookingsReq->execute();
-        while($booking = $bookingsReq->fetch(PDO::FETCH_ASSOC)){ ?>
-            <article class="col-12 col-md-4 mb-3">
-                <div class="card h-100">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                        <div class="mb-3">
-                            <h4 class="card-title text-gold"><?= $booking['name'] ?></h4>
-                            <p class="card-text mb-2 text-lgrey">Suite <?= $booking['title'] ?></p>
-                            <p class="card-text">Arrivée : <?= $booking['startDate'] ?></p>
-                            <p class="card-text">Départ : <?= $booking['endDate'] ?></p>
-                        </div>
-                        <!-- SI ON PEUT ANNULER, AFFICHER BOUTON -->
-                        <?php
-                        $today = date('%d/%m/%Y');
-                        if($today < $booking['cancellationDate']){ ?>
-                            <div class="text-end">
-                                <a type="button" href="#" class="btn bg-offwhite border-gold rounded-pill text-gold px-5">Annuler</a>
+        $count = $bookingsReq->rowCount();
+        if($count ===0){ ?>
+            <h2 class="col-12 text-md-center text-tilered pb-5">Vous n'avez jamais réservé chez nous !</h2>
+        <?php } else {
+            while($booking = $bookingsReq->fetch(PDO::FETCH_ASSOC)){ ?>
+                <article class="col-12 col-md-4 mb-3">
+                    <div class="card h-100">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <div class="mb-3">
+                                <h4 class="card-title text-gold"><?= $booking['name'] ?></h4>
+                                <p class="card-text mb-2 text-lgrey">Suite <?= $booking['title'] ?></p>
+                                <p class="card-text">Arrivée : <?= $booking['startDate'] ?></p>
+                                <p class="card-text">Départ : <?= $booking['endDate'] ?></p>
                             </div>
-                        <?php }
-                        ?>
+                            <!-- SI ON PEUT ANNULER, AFFICHER BOUTON -->
+                            <?php
+                            $today = date('Y-m-d');
+                            if($today < $booking['cancellationDate']){ ?>
+                                <div class="text-end">
+                                    <a type="button" href="./myBookings.php?delete=<?=$booking['id'] ?>" class="btn bg-offwhite border-gold rounded-pill text-gold px-5">Annuler</a>
+                                </div>
+                            <?php }
+                            ?>
+                        </div>
                     </div>
-                </div>
-            </article>
+                </article>
 
-        <?php }
+            <?php }
+        }
     ?>
 
 </section>
