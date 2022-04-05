17f47c0825fe7b7443b819997d5ceb3ee491ebaf
Soizic Venel <venel.soizic@hotmail.fr>
2022-04-05 15:40:48 +0200

    m

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 require_once('./components/db/db.php');
 if(isset($_SESSION['connect'])){
     if($_SESSION['connect'] == 'client'){
+        require('./aws/aws-autoloader.php');
         require_once('./components/header/header-client.php');
     } else if ($_SESSION['connect'] == 'pro'){
         require_once('./components/header/header-pro.php');
